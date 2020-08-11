11289cd4024aa4714534353f1ff997885849f27c
caueguedes <caue.guedes91@gmail.com>
2020-08-11 19:07:06 -0300

    adding relation belongs_to optinal default nil to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,13 @@
 class Order < ApplicationRecord
+  belongs_to :batch, optional: true, default: nil
+  enum status: { published: 0, production: 1, closing: 2, sent: 3}
+
   include Filterable
 
   scope :filter_by_status, -> (status){ where(status: status) }
   scope :filter_by_purchase_channel, -> (purchase_channel){ where(purchase_channel: purchase_channel) }
   scope :filter_by_reference, -> (reference){ where(purchase_channel: reference) }
   scope :filter_by_client_name, -> (client_name){ where(client_name: client_name) }
-
-  enum status: { published: 0, production: 1, closing: 2, sent: 3}
 
   validates :reference, uniqueness: true
   validates_presence_of :address, :client_name, :delivery_service, :line_items, :purchase_channel, :reference
