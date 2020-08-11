9f6613284a1cea0fd76b40d7773c8c8235790295
caueguedes <caue.guedes91@gmail.com>
2020-08-11 18:58:29 -0300

    Adding trait :has_enough_order, :without_enough_order to factory

--- spec/factories/batches.rb
+++ spec/factories/batches.rb
@@ -1,6 +1,31 @@
 FactoryBot.define do
   factory :batch do
-    reference { "201803-54" }
     purchase_channel { "Site BR" }
+
+    trait :have_enough_orders do
+      before(:build) do
+        create(:order, reference:"BR102030")
+        create(:order, reference:"BR102031")
+        create(:order, reference:"BR102032", purchase_channel:'Site USD')
+        create(:order, reference:"BR102033", status: 3)
+        create(:order, reference:"BR102034", status: 2)
+        create(:order, reference:"BR102035", status: 2)
+        create(:order, reference:"BR102036", status: 2, purchase_channel:'Web BR')
+        create(:order, reference:"BR102037", status: 2, purchase_channel:'Site USD')
+      end
+    end
+
+    trait :without_enough_orders do
+      before(:build) do
+        create(:order, reference:"BR102030")
+        create(:order, reference:"BR102031")
+        create(:order, reference:"BR102032", purchase_channel:'Site USD')
+        create(:order, reference:"BR102033", status: 3)
+        create(:order, reference:"BR102034", status: 2)
+        create(:order, reference:"BR102035", status: 2)
+        create(:order, reference:"BR102036", status: 2, purchase_channel:'Web BR')
+        create(:order, reference:"BR102037", status: 2, purchase_channel:'Site USD')
+      end
+    end
   end
 end
