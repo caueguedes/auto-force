958cc7fd93c68bc2fdd934e78a2bec6677d389e9
caueguedes <caue.guedes91@gmail.com>
2020-08-12 23:51:10 -0300

    ActionController::ParameterMissing rescue from treatment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::ParameterMissing, :with => :parameter_missing
+
+  private
+    def parameter_missing(_e)
+      entity = _e.param.to_s
+      render json: { "#{entity}": "#{entity} param is missing"}, status: :unprocessable_entity
+    end
 end
