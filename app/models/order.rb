4bafa5db5b014bc77326bb2065fec15331574939
caueguedes <caue.guedes91@gmail.com>
2020-08-12 23:59:46 -0300

    filter fix to model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
 
   scope :filter_by_status, -> (status){ where(status: status) }
   scope :filter_by_purchase_channel, -> (purchase_channel){ where(purchase_channel: purchase_channel) }
-  scope :filter_by_reference, -> (reference){ where(purchase_channel: reference) }
+  scope :filter_by_reference, -> (reference){ where(reference: reference) }
   scope :filter_by_client_name, -> (client_name){ where(client_name: client_name) }
 
   validates :reference, uniqueness: true
