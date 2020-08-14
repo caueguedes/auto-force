c43a0f65652fe8ab3a94fb1aaa5b6c0f2ea3a73b
caueguedes <caue.guedes91@gmail.com>
2020-08-13 21:24:45 -0300

    Model Order refactored, behavior moved to concern OrderValidator

--- app/models/concerns/order_validator.rb
+++ app/models/concerns/order_validator.rb
@@ -1,4 +1,14 @@
 module OrderValidator
   extend ActiveSupport::Concern
 
+  included do
+    validates :reference,         uniqueness: true
+    validates :reference,         presence: true
+    validates :address,           presence: true
+    validates :client_name,       presence: true
+    validates :delivery_service,  presence: true
+    validates :purchase_channel,  presence: true
+    validates :line_items,        presence: true
+    validates :total_value, numericality: { greater_than_or_equal_to: 0}
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,13 @@
 class Order < ApplicationRecord
-  belongs_to :batch, optional: true, default: nil
-  enum status: { published: 0, production: 1, closing: 2, sent: 3}
-
   include Filterable
+  include OrderValidator
+
+  belongs_to :batch, optional: true, default: nil
+
+  enum status: { published: 0, production: 1, closing: 2, sent: 3}
 
   scope :filter_by_status, -> (status){ where(status: status) }
   scope :filter_by_purchase_channel, -> (purchase_channel){ where(purchase_channel: purchase_channel) }
   scope :filter_by_reference, -> (reference){ where(reference: reference) }
   scope :filter_by_client_name, -> (client_name){ where(client_name: client_name) }
-
-  validates :reference, uniqueness: true
-  validates_presence_of :address, :client_name, :delivery_service, :line_items, :purchase_channel, :reference
-  validates :total_value, numericality: { greater_than_or_equal_to: 0}
 end
