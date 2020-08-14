4f4615af535742db257902696b4ee6c9ee433fe3
caueguedes <caue.guedes91@gmail.com>
2020-08-13 23:49:03 -0300

    Model Batch refactored, added validation behavior on batch_validator

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,11 +1,7 @@
 class Batch < ApplicationRecord
   has_many :orders, autosave: true
 
-  before_validation :set_orders_to_batch, on: :create
-  before_validation :validate_has_orders, on: :create
-  before_validation :set_reference
-  validates :reference, uniqueness: true
-  after_save :change_status_published_to_production
+  include BatchValidator
 
   def produce
     orders.update(status: :closing)
@@ -17,25 +13,6 @@ class Batch < ApplicationRecord
 
   def change_status_published_to_production
     orders.update status: :production  #if orders.first.published?
-  end
-
-  def set_orders_to_batch
-    self.orders= Order.where(purchase_channel: self.purchase_channel).published
-  end
-
-  def validate_has_orders
-    errors.add(:orders, ", not enough orders to create a batch") if orders.empty?
-  end
-
-  def set_reference
-    self.reference = generate_reference
-  end
-
-  def generate_reference
-    loop do
-      reference = SecureRandom.hex(6)
-      break reference unless Batch.where(reference: reference).exists?
-    end
   end
 end
 # https://makandracards.com/alexander-m/39365-generate-unique-random-token
--- app/models/concerns/batch_validator.rb
+++ app/models/concerns/batch_validator.rb
@@ -1,4 +1,32 @@
 module BatchValidator
   extend ActiveSupport::Concern
 
+  included do
+    before_validation :set_orders_to_batch, on: :create
+    before_validation :validate_has_orders, on: :create
+
+    before_validation :set_reference
+    validates :reference, uniqueness: true
+
+    after_save :change_status_published_to_production
+  end
+
+  def set_orders_to_batch
+    self.orders= Order.where(purchase_channel: self.purchase_channel).published
+  end
+
+  def validate_has_orders
+    errors.add(:orders, ", not enough orders to create a batch") if orders.empty?
+  end
+
+  def set_reference
+    self.reference = generate_reference
+  end
+
+  def generate_reference
+    loop do
+      reference = SecureRandom.hex(6)
+      break reference unless Batch.where(reference: reference).exists?
+    end
+  end
 end
