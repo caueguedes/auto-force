39c9cbaeadd2604dc41a509d121a7aa67074da93
caueguedes <caue.guedes91@gmail.com>
2020-08-16 18:13:41 -0300

    Pagination letting set paginate param public

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -1,6 +1,11 @@
 module Pagination
   extend ActiveSupport::Concern
 
+  def set_paginate_params
+    @page = params[:current_page] || 1
+    @per_page = params[:per_page] || 10
+  end
+
   private
     def paginate(entity)
       entity_data = entity.page(@page).per(@per_page)
@@ -16,10 +21,5 @@ module Pagination
             total_pages: total_pages
         }
       }
-    end
-
-    def set_paginate_params
-      @page = params[:current_page] || 1
-      @per_page = params[:per_page] || 10
     end
 end
