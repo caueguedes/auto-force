841fe7740fe4927a852009981f0c803ab7dd212a
caueguedes <caue.guedes91@gmail.com>
2020-08-16 18:18:44 -0300

    moving behavior from model to services

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,17 +2,8 @@ class Batch < ApplicationRecord
   has_many :orders, autosave: true
 
   include BatchValidator
+  include Filterable
 
-  def produce
-    orders.update(status: :closing)
-  end
-
-  def close(delivery_service)
-    orders.where(delivery_service: delivery_service).each {| order|  order.sent!}
-  end
-
-  def change_status_published_to_production
-    orders.update status: :production  #if orders.first.published?
-  end
+  scope :filter_by_reference, -> (reference){ where(reference: reference) }
 end
 # https://makandracards.com/alexander-m/39365-generate-unique-random-token
