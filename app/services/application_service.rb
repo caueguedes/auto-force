0bdafd13158b6dd72ed48c94c2e2039c36d59671
caueguedes <caue.guedes91@gmail.com>
2020-08-16 18:16:47 -0300

    Application service fix and classes error and success defined as return from services

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,9 +1,6 @@
 class ApplicationService
-  extend ActiveSupport::Concern
 
-  class_methods do
-    def call(*args, &block)
-      new(*args, &block).call
-    end
+  def self.call(*args, &block)
+    new(*args, &block).call
   end
 end
---
+++ app/services/error.rb
@@ -0,0 +1,10 @@
+class Error
+  attr_reader :errors
+  def initialize(errors)
+    @errors = errors
+  end
+
+  def success?
+    false
+  end
+end
