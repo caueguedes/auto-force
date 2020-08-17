faf3308ca38a0a3f6fba9710c4956cfbf49187bc
caueguedes <caue.guedes91@gmail.com>
2020-08-17 01:57:11 -0300

    removing duplicated file financial_controller

--- app/controllers/financial_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::FinancialController < ApplicationController
-
-  def report
-    report = Order.select('SUM(total_value) as total_value, COUNT(id) as total_orders', :purchase_channel)
-                 .group(:purchase_channel).map { |order| order.as_json.except :id }
-    render json: report, status: :ok
-  end
-end
